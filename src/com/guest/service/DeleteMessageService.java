acbbf667a66fe14095315ae268def4b4d020a771
sebaek <sebaek@gmail.com>
2020-07-22 15:43:14 +0900

    delete service

--- src/com/guest/service/DeleteMessageService.java
+++ src/com/guest/service/DeleteMessageService.java
@@ -1,5 +1,47 @@
 package com.guest.service;
 
-public class DeleteMessageService {
+import java.sql.Connection;
 
+import com.guest.dao.MessageDao;
+import com.guest.jdbc.ConnectionProvider;
+import com.guest.jdbc.JdbcUtil;
+import com.guest.model.Message;
+
+public class DeleteMessageService {
+	private static DeleteMessageService instance = new DeleteMessageService();
+	
+	public static DeleteMessageService getInstance() {
+		return instance;
+	}
+	
+	private DeleteMessageService() {
+		
+	}
+	
+	public String deleteMessage(int messageId, String password) {
+		Connection conn = null;
+		
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			
+			MessageDao messageDao = MessageDao.getInstance();
+			Message message = messageDao.select(conn, messageId);
+			
+			if (message == null) {
+				return "메시지 없음";
+			}
+			if (message.matchPassword(password)) {
+				return "패스워드 불일치";
+			}
+			messageDao.delete(conn, messageId);
+			conn.commit();
+		} catch (Exception e) {
+			JdbcUtil.rollback(conn);
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(conn);
+		}
+		return "삭제 성공";
+	}
 }
