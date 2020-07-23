e729aab9daa8bcc98b60cff8e47d2682356df22a
sebaek <sebaek@gmail.com>
2020-07-23 12:04:58 +0900

    delete

--- src/com/guest/servlet/DeleteServlet.java
+++ src/com/guest/servlet/DeleteServlet.java
@@ -1,11 +1,15 @@
 package com.guest.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.guest.service.DeleteMessageService;
 
 /**
  * Servlet implementation class DeleteServlet
@@ -34,16 +38,24 @@ public class DeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String messageIdStr = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		int messageId = Integer.valueOf(messageIdStr);
+		
 		// delete service 객체 얻어서
+		DeleteMessageService service = DeleteMessageService.getInstance();
 		
 		// delete 메소드 실행
-		
+		String info = service.deleteMessage(messageId, password);
 		// 결과 string을
 		
 		// session "info" attribute로 셋팅
+		session.setAttribute("info", info);
 		
 		// main 으로 redirection
-	
+		response.sendRedirect("main");
 	}
 
 }
